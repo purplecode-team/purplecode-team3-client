3d499f22fe952df180630d5eb727560ae4894cc7
JangJua <jua0610@naver.com>
2021-02-22 11:03:07 +0900

    fix: dir 위치 변경

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -3,7 +3,7 @@ import BasicTemplate from "../containers/common/BasicTemplate";
 import Header from "../containers/main/Header";
 import Search from "../containers/main/Search";
 import Category from "../containers/main/Category";
-import TrendingAuctions from "../containers/TrendingAuctions";
+import TrendingAuctions from "../containers/main/TrendingAuctions";
 import RecentAuctions from "../containers/main/RecentAuctions";
 import Footer from "../containers/main/Footer";
 
