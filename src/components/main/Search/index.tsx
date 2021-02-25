c4aa35f176f16067fd421bd90f8ac51607e9ec22
DESKTOP-G3G8GKG\psy <sooy8951@gmail.com>
2021-02-26 01:52:58 +0900

    add: 검색 기능 추가

--- src/components/main/Search/index.tsx
+++ src/components/main/Search/index.tsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import * as S from './style';
+import importedData from './dummy.json';
 
-function Search() {
+
+function Search( ) {
+  const [value, setValue] = useState('');
+  const onChange= (e:React.ChangeEvent<HTMLInputElement>) =>{
+    setValue(e.target.value);
+  };
+  const onSubmit = ()=> {
+    setValue('');
+    /* value : 찾아야하는 키워드 */
+    const dataArray = importedData.data;
+    const filteredData = dataArray.filter((d:any)=>
+      (d.title && d.title.toLowerCase().includes(value.toLowerCase()))
+    );
+    /* 결과 확인용 임시 콘솔출력 */
+    console.log(filteredData);
+
+  };
+
   return (
-    <h2> Search </h2>
+    <S.SearchWrapper>
+      <S.SearchInput
+        placeholder="실시간 매물 정보를 검색해보세요."
+        type="text"
+        value={value}
+        onChange={onChange}
+      />
+      <Link to='/'><S.SearchIcon onClick={onSubmit} type="submit"  /></Link>
+    </S.SearchWrapper>
   );
 }
+
 
 export default Search;
