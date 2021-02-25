cb534aa47615525ba615c95107fe6762355537d4
JangJua <jua0610@naver.com>
2021-02-26 00:38:39 +0900

    add: 곧 시작하는 경매 컴포넌트 추가

--- src/components/main/TrendingAuctions/index.tsx
+++ src/components/main/TrendingAuctions/index.tsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import React from "react";
+import * as S from "./style";
+import dummy from "./dummy.json";
+import AuctionItem from "../AuctionItem";
 
-function TrendingActions() {
-  return (
-    <h2> Trending Actions </h2>
-  );
+export interface Image {
+  id: number;
+  imagePath: string;
 }
 
-export default TrendingActions;
+export interface Auction {
+  id: number;
+  title: string;
+  description: string;
+  startPrice: number;
+  startDate: string;
+  images: Image[];
+}
+
+const TrendingAuctions = () => (
+  <S.Container>
+    <S.Title>
+      <span> HOT! </span> 곧 시작하는 경매
+    </S.Title>
+    <S.AuctionsContainer>
+      {dummy.data &&
+        dummy.data.map(
+          ({
+            id,
+            title,
+            description,
+            startPrice,
+            startDate,
+            images,
+          }: Auction) => (
+            <AuctionItem
+              key={id}
+              title={title}
+              description={description}
+              startPrice={startPrice}
+              startDate={startDate}
+              thumbnail={images[0].imagePath}
+            />
+          )
+        )}
+    </S.AuctionsContainer>
+  </S.Container>
+);
+
+export default TrendingAuctions;
