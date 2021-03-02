c6483f394fbd335b555907343f63f8b5329b4593
annie1004619 <annie1004619@naver.com>
2021-03-02 22:36:08 +0900

    add: HamburgerMenu안에 들어가는 컴포넌트들

---
+++ src/components/main/Header/MainButton.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import dummy from "./dummyUser.json";
+import * as S from "./style";
+
+function MainButton() {
+  return (
+    <S.MainButton>
+      {dummy.user ? (
+        <S.StyleLink to="/myaution">나의 경매 바로가기</S.StyleLink>
+      ) : (
+        <S.StyleLink to="/login">로그인 하러 가기</S.StyleLink>
+      )}
+    </S.MainButton>
+  );
+}
+
+export default MainButton;
